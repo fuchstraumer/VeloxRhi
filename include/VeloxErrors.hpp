6998015aac200921bd0c0d43ccad0a8081e21592
Fuchs <fuchstraumer@gmail.com>
2026-08-08 23:10:39 -0700

    Add missing error for shader module failure

--- include/VeloxErrors.hpp
+++ include/VeloxErrors.hpp
@@ -35,6 +35,7 @@ enum class RhiError : uint64_t
     BufferMapFailed = 17,
     PipelineCreationFailed = 18,
     BootstrapInInvalidState = 19,
+    ShaderModuleCreationFailed = 20,
     // Start async function specific error codes (as needed)
     AsyncCallbackCanceled = 50,
     AsyncBufferMapAborted = 55,
