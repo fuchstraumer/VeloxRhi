31c6c6d537d03a66176f5050f4f68be17932da1c
Fuchs <fuchstraumer@gmail.com>
2026-08-08 18:27:24 -0700

    Future<T> implementation
    Now this holds the promise type, and is our bus for the async events. By implementing the functions in the source the way we do, callers get to treat themselves as regular synchronous functions that just query the future.

--- include/Future.hpp
+++ include/Future.hpp
@@ -8,42 +8,110 @@ namespace velox
 
 struct Scheduler;
 
-/* Maybe need a beter name, but this is the type returned to callers of our async functions. */
+/**Brainblast moment: this is what holds the promise type. This is what defines the type of the result, what
+ * actually carries some type information, and also what owns the coroutine_handle. Scheduler is not just a
+ * big pile of type-erased coroutine handles. This simplifies awaitable dispatching tremendously.
+ *
+ */
 template<typename T>
-class Future
+struct Future
 {
-    Scheduler* scheduler;
-    SlotMapHandle handle;
+    struct promise_type
+    {
+        Result<T> result_value;
 
-public:
-    std::optional<Result<T>> TryGet();
+        Future<T> get_return_object() noexcept
+        {
+            return Future<T>(std::coroutine_handle<promise_type>::from_promise(*this));
+        }
+
+        // don't suspend initially, because we call the async function and let the awaitable
+        // that constructs be what first sends us into suspension (after enqueuing our action)
+        constexpr std::suspend_never initial_suspend() noexcept
+        {
+            return {};
+        }
+
+        // always suspend at the end so we can extract the result before destruction
+        constexpr std::suspend_always final_suspend() noexcept
+        {
+            return {};
+        }
+
+        template<typename U>
+        void return_value(U&& value) noexcept(std::is_nothrow_constructible_v<T, U&&>)
+        {
+            result_value = std::forward<U>(value);
+        }
+
+        void unhandled_exception()
+        {
+#ifndef __EMSCRIPTEN__
+            std::terminate();
+#else
+            emscripten_force_exit(1);
+#endif
+        }
+    };
+
+    std::coroutine_handle<promise_type> handle;
+
+    explicit Future(std::coroutine_handle<promise_type> _handle) noexcept
+        : handle{ _handle }
+    {
+    }
+
+    ~Future()
+    {
+        if (handle)
+        {
+            handle.destroy();
+        }
+    }
+
+    Future(const Future&) = delete;
+    Future& operator=(const Future&) = delete;
+    Future(Future&& other) noexcept = default;
+    Future& operator=(Future&& other) noexcept = default;
+
+    // because this now holds the coroutine and promise, this simplifies tremendously
+    std::optional<Result<T>> TryGet()
+    {
+        // simple as can be: if handle isn't done, return nullopt. awesome.
+        if (!handle || !handle.done())
+        {
+            return std::nullopt;
+        }
+
+        Result<T> value = std::move(handle.promise().result_value);
+        // now clean up coroutine, as we're totally done
+        handle.destroy();
+        handle = nullptr;
+        return value;
+    }
 };
 
-using RenderPipelineFuture = Future<Result<wgpu::RenderPipeline>>;
-using ComputePipelineFuture = Future<Result<wgpu::ComputePipeline>>;
+using AdapterFuture = Future<wgpu::Adapter>;
+using DeviceFuture = Future<wgpu::Device>;
+using RenderPipelineFuture = Future<wgpu::RenderPipeline>;
+using ComputePipelineFuture = Future<wgpu::ComputePipeline>;
 using MapReadFuture = Future<Result<const void*>>;
 using MapWriteFuture = Future<Result<void*>>;
 
-RenderPipelineFuture RequestRenderPipeline(Scheduler* _ctxt,
-                                           wgpu::Device _device,
-                                           wgpu::RenderPipelineDescriptor _descr);
+AdapterFuture RequestAdapter(wgpu::Instance _instance,
+                             wgpu::RequestAdapterOptions _options,
+                             Scheduler* _scheduler);
 
-ComputePipelineFuture RequestComputePipeline(Scheduler* _ctxt,
-                                             wgpu::Device _device,
-                                             wgpu::ComputePipelineDescriptor _descr);
+DeviceFuture RequestDevice(wgpu::Adapter _adapter, wgpu::DeviceDescriptor _descr, Scheduler* _scheduler);
 
-MapReadFuture RequestMapBufferReadAsync(Scheduler* _ctxt,
-                                        wgpu::Device _device,
-                                        wgpu::Buffer _buffer,
-                                        size_t size,
-                                        size_t offset = 0u);
+RenderPipelineFuture RequestRenderPipeline(wgpu::Device _device,
+                                           wgpu::RenderPipelineDescriptor _descr,
+                                           Scheduler* _scheduler);
 
-MapWriteFuture RequestMapBufferWritesync(Scheduler* _ctxt,
-                                         wgpu::Device _device,
-                                         wgpu::Buffer _buffer,
-                                         size_t size,
-                                         size_t offset = 0u);
+ComputePipelineFuture RequestComputePipeline(wgpu::Device _device,
+                                             wgpu::ComputePipelineDescriptor _descr,
+                                             Scheduler* _scheduler);
 
-}
+} // namespace velox
 
 #endif // !VELOX_ASYNC_FUTURE_HPP
--- src/Future.cpp
+++ src/Future.cpp
@@ -1,15 +1,47 @@
 #include "Future.hpp"
+#include "Scheduler.hpp"
 
 namespace velox
 {
 
-RenderPipelineFuture RequestRenderPipeline(Context* _ctxt,
-                                                                    wgpu::Device _device,
-                                                                    wgpu::RenderPipelineDescriptor _descr)
+AdapterFuture RequestAdapater(wgpu::Instance _instance,
+                              wgpu::RequestAdapterOptions _options,
+                              Scheduler* _scheduler)
 {
-    auto coroutine = [](Context* ctxt, wgpu::Device _device, wgpu::RenderPipelineDescriptor _descr)
-    {
-        
-    }
+    auto result = co_await AdapterAwaitable{ _instance,
+                                            std::forward<wgpu::RequestAdapterOptions>(_options),
+                                            _scheduler };
+    co_return result;
 }
+
+DeviceFuture RequestDevice(wgpu::Adapter _adapter,
+                           wgpu::DeviceDescriptor _descriptor,
+                           Scheduler* _scheduler)
+{
+    auto result = co_await DeviceAwaitable{ _adapter,
+                                            std::forward<wgpu::DeviceDescriptor>(_descriptor),
+                                            _scheduler };
+    co_return result;
 }
+
+RenderPipelineFuture RequestRenderPipeline(wgpu::Device _device,
+                                           wgpu::RenderPipelineDescriptor _descr,
+                                           Scheduler* _scheduler)
+{
+    auto result = co_await RenderPipelineAwaitable{  _device,
+                                                     std::forward<wgpu::RenderPipelineDescriptor>(_descr),
+                                                     _scheduler };
+    co_return result;
+}
+
+ComputePipelineFuture RequestComputePipeline(wgpu::Device _device,
+                                             wgpu::ComputePipelineDescriptor _descriptor,
+                                             Scheduler* _scheduler)
+{
+    auto result = co_await ComputePipelineAwaitable{ _device,
+                                                     std::forward<wgpu::ComputePipelineDescriptor>(_descriptor),
+                                                     _scheduler };
+    co_return result;
+}
+
+} // namespace velox
